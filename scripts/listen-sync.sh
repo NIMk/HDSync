df9cdb5cd00523f14df8919b0511e721c0a3f614
Jaromil <jaromil@dyne.org>
2010-06-15 11:52:24 +0200

    listener now starts play

--- scripts/listen-sync.sh
+++ scripts/listen-sync.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
 if [ -z $1 ]; then
 	echo "usage: $0 network_interface"
 	echo "example: $0 eth0"
@@ -15,17 +17,43 @@ else
     BC="$APPROOT/bin/broadcaster"
 fi
 
-# some more version might be around that is not supported..
 
-IP="`/sbin/ifconfig $IFACE | grep 'inet addr'| awk '{print $2}'|cut -f2 -d:`"
 
-echo "listening on $IFACE configured with address $IP ..."
+IP="`ifconfig $IFACE | grep 'inet addr'| awk '{print $2}'|cut -f2 -d:`"
+
+echo "listening on $IFACE configured with address $IP ..." 
 master="`echo | $NC -c -u -l -p 3332`"
 
 echo "contacted by master $master"
 echo "$IP" | $NC -u $master 3331
 
-echo "master replied"
+# poor man's syncstarting:
+# emulating remote control commands
+#
+# we could do much better if this damn Sigma SDK would be open
+# but so far, so good.
+
+# go to the video
+Echo "r" > /tmp/ir_injection; sleep 0.333
+Echo "r" > /tmp/ir_injection; sleep 0.333
+Echo "r" > /tmp/ir_injection; sleep 0.333
+# play it
+Echo "p" > /tmp/ir_injection; sleep 1
+Echo "# be sure we restart the video"
+Echo "n" > /tmp/ir_injection; sleep 1
+Echo "# wait 5 secs"
+Sleep 5
+Echo "# pause it"
+Echo "p" > /tmp/ir_injection
+ 
+# exit after connection
+$NC -u -l -p 3333 -e true
+ 
+# "press play on tape"
+Echo "p" > /tmp/ir_injection; sleep 0.1
+# take off OSD
+Echo "n" > /tmp/ir_injection
+ 
 
 
 exit 0
--- scripts/offer-sync.sh
+++ scripts/offer-sync.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
 if [ -z $1 ]; then
         echo "usage: $0 network_interface"
         echo "example: $0 eth0"
@@ -7,7 +9,7 @@ if [ -z $1 ]; then
 fi
 IFACE="$1"
 
-IP="`/sbin/ifconfig $IFACE | grep 'inet addr'| awk '{print $2}'|cut -f2 -d:`"
+IP="`ifconfig $IFACE | grep 'inet addr'| awk '{print $2}'|cut -f2 -d:`"
 
 if [ -z $APPROOT ]; then
     NC="../src/netcat -c"
@@ -17,22 +19,22 @@ else
     BC="$APPROOT/bin/broadcaster"
 fi
 
-
-
-
 ready=false
+
+rm -f /tmp/handshake.ok
+echo "broadcasting sync signal"
 
 # background listener
 (answer=`echo | $NC -u -l -p 3331`;
-    echo $answer > /tmp/handshake.$i.ok) &
+    echo $answer > /tmp/handshake.ok) &
 
-while ! [ -r /tmp/handshake.$i.ok ]; do
+while ! [ -r /tmp/handshake.ok ]; do
     sleep 1
-    $BC 255.255.255.255 3332 $IP 1>&2 > /dev/null
-    echo -n "."
-done	
-echo -n " answer: `cat /tmp/handshake.$i.ok`"
-rm /tmp/handshake.$i.ok
+    echo -n "[`date +%X`] "
+    $BC 255.255.255.255 3332 $IP
+done
+
+echo -n " answer: `cat /tmp/handshake.ok`"
 echo
 
 
