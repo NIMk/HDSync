997a38b15a3363d0aeac9a4bb9b3de97a875f7f3
Jaromil <jaromil@dyne.org>
2010-06-24 16:44:54 +0200

    assemble-app script now has update function

--- scripts/assemble-app-bin.sh
+++ scripts/assemble-app-bin.sh
@@ -22,38 +22,64 @@
 # assembly script for application images
                  
 appname=hdsync
-imagefile=./$appname.app.bin
-loopdir=./$appname.app.loop
 
+if [ -z $1 ]; then
+    # no argument, create fresh
+
+    imagefile=./$appname.app.bin
+    loopdir=./$appname.app.loop
+    
 # create and fill in appdir
-appdir=$appname.app
+    appdir=$appname.app
+    
+    sudo rm -rf $appdir $imagefile $loopdir
+    
+    mkdir -p $appdir
+    mkdir -p $appdir/bin
+    mkdir -p $appdir/etc/init.d
+    
+    cp -v src/netcat $appdir/bin &&
+    cp -v src/broadcaster $appdir/bin &&
+    strip $appdir/bin/* &&
+    cp -v scripts/*-sync.sh $appdir/bin &&
+    cp -v scripts/S88hdsync $appdir/etc/init.d &&
+    chmod a+x $appdir/etc/init.d/S88hdsync &&
+    cp -v README $appdir &&
+    
+    sudo chown -R root:root $appdir
+    
+    dd if=/dev/zero of=$imagefile bs=1K count=500 &&
+    /sbin/mkfs.ext3 -F $imagefile &&
+    /sbin/tune2fs -c 0 -i 0 $imagefile &&
+    mkdir -p $loopdir &&
+    sudo mount -o loop $imagefile $loopdir &&
+    cp -a $appdir/* $loopdir/ &&
+    sudo rm -rf $loopdir/lost+found &&
+    sudo chown root:root -R $loopdir &&
+    sudo umount $loopdir &&
+    sudo rm -rf $loopdir $appdir && 
+    sudo /sbin/fsck.ext3 $imagefile
 
-sudo rm -rf $appdir $imagefile $loopdir
+else
+    # has argument, update existing
+    appname=$1
+    loopdir=/tmp/`basename $appname`.loop
 
-mkdir -p $appdir
-mkdir -p $appdir/bin
-mkdir -p $appdir/etc/init.d
+    if ! [ -r $appname ]; then
+	echo "error: $appname doesn't exist"
+	exit 1
+    fi
+    mkdir -p $loopdir
+    sudo mount -o loop $appname $loopdir
+    sudo cp -v scripts/*-sync.sh $loopdir/bin/
+    sudo cp -v scripts/S88hdsync $loopdir/etc/init.d &&
+    sudo chmod a+x $loopdir/etc/init.d/S88hdsync &&
+    sudo cp -v README AUTHORS $loopdir &&
 
-cp -v src/netcat $appdir/bin &&
-cp -v src/broadcaster $appdir/bin &&
-strip $appdir/bin/* &&
-cp -v scripts/*-sync.sh $appdir/bin &&
-cp -v scripts/S88hdsync $appdir/etc/init.d &&
-chmod a+x $appdir/etc/init.d/S88hdsync &&
-cp -v README $appdir &&
+    sudo chown -R root:root $loopdir
 
-sudo chown -R root:root $appdir
+    sudo umount $loopdir
+    sudo sync
+    sudo rm -r $loopdir
 
-dd if=/dev/zero of=$imagefile bs=1K count=500 &&
-/sbin/mkfs.ext3 -F $imagefile &&
-/sbin/tune2fs -c 0 -i 0 $imagefile &&
-mkdir -p $loopdir &&
-sudo mount -o loop $imagefile $loopdir &&
-cp -a $appdir/* $loopdir/ &&
-sudo rm -rf $loopdir/lost+found &&
-sudo chown root:root -R $loopdir &&
-sudo umount $loopdir &&
-sudo rm -rf $loopdir $appdir && 
-sudo /sbin/fsck.ext3 $imagefile
-
-
+fi
